329f864bdcf2a607266cf863475056039436deae
LUGEMWA juliuslugemwa42@gmail.com <137568277+Luguspro@users.noreply.github.com>
2023-12-08 10:21:30 +0300

    Update lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-def print_list_integer(my_list = []):
+def print_list_integer(my_list=[]):
+    Pass
 def element_at(my_list, idx):
+    pass
 def replace_in_list(my_list, idx, element):
-def print_reversed_list_integer(my_list = []):
+    pass
+def print_reversed_list_integer(my_list=[]):
+    pass
 def new_in_list(my_list, idx, element):
+    pass
 def no_c(my_string):
-def print_matrix_integer(matrix = [[]]):
+    pass
+def print_matrix_integer(matrix=[[]]):
+    pass
